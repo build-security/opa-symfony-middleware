fd88d23f8da273a940194db113d0ed875c962af8
Yash Tewari <yashtewari1996@gmail.com>
2021-02-17 18:47:53 +0530

    Updated example with read/write requirements.

--- example/app/src/Controller/Controller.php
+++ example/app/src/Controller/Controller.php
@@ -6,15 +6,23 @@ use Symfony\Component\HttpFoundation\Response;
 use BuildSecurity\OpenPolicyAgentBundle\Authorize;
 
 
-class LuckyController
+class Controller
 {
-    #[Authorize('request.label')]
-    public function number(): Response
+    #[Authorize('read', 'write')]
+    public function randomNumber(): Response
     {
         $number = random_int(0, 100);
 
         return new Response(
             '<html><body>Lucky number: '.$number.'</body></html>'
+        );
+    }
+
+    #[Authorize('read')]
+    public function constantNumber(): Response
+    {
+        return new Response(
+            '<html><body>Just a number: 42 </body></html>'
         );
     }
 }
