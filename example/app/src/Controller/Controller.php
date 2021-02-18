6324efe82a7a72c67984572d8c34734e4d3e5f5b
Yash Tewari <yashtewari1996@gmail.com>
2021-02-18 20:51:27 +0530

    Improve example app. (#3)

--- example/app/src/Controller/Controller.php
+++ example/app/src/Controller/Controller.php
@@ -8,21 +8,38 @@ use BuildSecurity\OpenPolicyAgentBundle\Authorize;
 
 class Controller
 {
-    #[Authorize('read', 'write')]
-    public function randomNumber(): Response
-    {
-        $number = random_int(0, 100);
-
+    #[Authorize('blog.view')]
+    public function viewBlog(string $user, string $blog_slug) {
         return new Response(
-            '<html><body>Lucky number: '.$number.'</body></html>'
+            'You are viewing '.$blog_slug.' by '.$user
         );
     }
 
-    #[Authorize('read')]
-    public function constantNumber(): Response
-    {
+    #[Authorize('blog.create')]
+    public function createBlog(string $blog_slug) {
         return new Response(
-            '<html><body>Just a number: 42 </body></html>'
+            'You are creating '.$blog_slug
+        );
+    }
+
+    #[Authorize('blog.edit')]
+    public function editBlog(string $blog_slug) {
+        return new Response(
+            'You are editing '.$blog_slug
+        );
+    }
+
+    #[Authorize('blog.delete')]
+    public function deleteBlog(string $blog_slug) {
+        return new Response(
+            'You are deleting '.$blog_slug
+        );
+    }
+
+    #[Authorize('admin_console.view')]
+    public function viewAdminConsole() {
+        return new Response(
+            'You are viewing the admin console.'
         );
     }
 }
